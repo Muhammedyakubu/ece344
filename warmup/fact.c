b1203e6cc18dda1754980c5156bd2ff2c37a463d
Muhammed Yakubu <m.yakubu@mail.utoronto.ca>
2022-09-15 16:31:38 -0400

    completed fact.c

--- warmup/fact.c
+++ warmup/fact.c
@@ -1,8 +1,37 @@
 #include "common.h"
+#include <ctype.h>
 
-int
-main()
+int fact(int n){
+	if(n==0 || n==1)
+		return n;
+	return n * fact(n-1);
+}
+
+int main(int argc, char *argv[])
 {
-	TBD();
+	if(argc < 2) {
+		printf("Huh?\n");
+		return 0;
+	}
+
+	// check that input is a valid int
+	char *s = argv[1];
+
+	// traverse the input string until we find a non-digit
+	for(int i = 0; s[i] != '\0'; i++){
+		if (!isdigit(s[i])) {
+			printf("Huh?\n");
+			return 0;
+		}
+	}
+	
+	int num = atoi(argv[1]);
+
+	if (num > 12) {
+		printf("Overflow\n");
+		return 0;
+	}
+
+	printf("%d\n", fact(num));
 	return 0;
 }
