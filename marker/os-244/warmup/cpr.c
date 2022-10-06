fbe9e151684d6aa9d57fa44db22efe8fcb10ef0c
Muhammed Yakubu <m.yakubu@mail.utoronto.ca>
2022-10-06 12:41:22 -0400

    committing uncommitted changes lol

---
+++ marker/os-244/warmup/cpr.c
@@ -0,0 +1,172 @@
+#include <errno.h>
+#include <string.h>
+#include "common.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <dirent.h>
+/* make sure to use syserror() when a system call fails. see common.h */
+
+void
+usage()
+{
+	fprintf(stderr, "Usage: cpr srcdir dstdir\n");
+	exit(1);
+}
+
+// TODO: modify this function to apply permissions from src to dst
+int
+copy_file(char *dst, char *src) {
+	// open the src file
+	int fd_src;
+	if ((fd_src = open(src, O_RDONLY)) < 0) {
+		syserror(open, src);
+	}
+
+	// create & open the dst file
+	int fd_dst;
+	if ((fd_dst = open(dst, O_RDWR | O_CREAT)) < 0) {
+		syserror(open, dst);
+	}
+	
+	// in a loop, read from the src file to the dst file
+	int bytes_read;
+	int BUF_SIZE = 4096;
+	char buf[BUF_SIZE];
+	while((bytes_read = read(fd_src, buf, BUF_SIZE)) > 0) {
+		if(write(fd_dst, buf, bytes_read) < 0) {
+			syserror(write, dst);
+		}
+	}
+
+	// error handling
+	if (bytes_read < 0) {
+		syserror(read, src);
+	}
+
+	// close src
+	close(fd_src);
+
+	// copy permissions from src file to the new file
+	struct stat src_st = {0};
+	if (stat(src, &src_st) == -1) {
+		syserror(stat, src);
+	}
+
+	chmod(dst, src_st.st_mode);
+	// close the new file
+	close(fd_dst);
+
+	return 1;
+}
+
+int
+copy_dir(char *dstdir, char *srcdir) {
+	// opendir srcdir 
+	DIR *src;
+	if((src = opendir(srcdir)) == NULL) {
+		syserror(opendir, srcdir);
+	}
+	// get src stat
+	struct stat src_st = {0};
+	if (stat(srcdir, &src_st) == -1) {
+		syserror(stat, srcdir);
+	}
+
+	// mkdir dstdir with all permissions, and change it at the end 
+	if(mkdir(dstdir, 0700) < 0) {
+		syserror(mkdir, dstdir);
+	}
+
+
+	// iteratively readdir srcdir, and duplicate the entries in dstdir
+	errno = 0;
+	struct dirent *entry = NULL;
+	struct stat e_stat = {0};
+
+	while((entry = readdir(src))) {
+		if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+			continue;
+		}
+
+		// construct the linux path for each entry in the srcdir
+		char entry_dstdir[256]; 
+		char entry_srcdir[256];
+
+		strcpy(entry_dstdir, dstdir);	
+		strcpy(entry_srcdir, srcdir);	
+
+		strcat(entry_dstdir, "/");
+		strcat(entry_srcdir, "/");
+
+		strcat(entry_dstdir, entry->d_name);
+		strcat(entry_srcdir, entry->d_name);
+
+		// is entry a dir or file? (stat)
+		// first get stat
+		if (stat(entry_srcdir, &e_stat) == -1) {
+			syserror(stat, entry_srcdir);
+		}
+
+		printf("Processing entry: %s in dir: %s with ISREG: %d & ISDIR: %d\n", entry->d_name, srcdir, S_ISREG(e_stat.st_mode), S_ISDIR(e_stat.st_mode));
+
+		if(S_ISREG(e_stat.st_mode)) {
+			// if it's a regular file, call copy_file
+			copy_file(entry_dstdir, entry_srcdir);
+			
+		} else if (S_ISDIR(e_stat.st_mode)) {
+			// recursively call copy_dir
+			copy_dir(entry_dstdir, entry_srcdir);
+		} else {
+			printf("Directory entry at %s was neither a file or directory", entry_srcdir);
+		}
+	}
+
+	// if errno was set then there was an error
+	if(errno) {
+		syserror(readdir, srcdir);
+	}
+
+	// closedir both dir 
+	closedir(src);
+	// set destination to the same mode as the srcdir
+	chmod(dstdir, src_st.st_mode);
+
+	return 0;
+}
+
+int
+main(int argc, char *argv[])
+{
+	if (argc != 3) {
+		usage();
+		return 0;
+	}
+
+	char *srcdir = argv[1];
+	char *dstdir = argv[2];
+
+	// printf("src is:%s and dst is:%s\n", srcdir, dstdir);
+
+	//	check if input is file or directory
+	struct stat src_st = {0};
+	if (stat(srcdir, &src_st) == -1) {
+		syserror(stat, srcdir);
+	}
+
+	
+	if(S_ISREG(src_st.st_mode)) {
+		// if it's a regular file, call copy_file
+		copy_file(dstdir, srcdir);
+		
+	} else if (S_ISDIR(src_st.st_mode)) {
+		// recursively call copy_dir
+		copy_dir(dstdir, srcdir);
+	} else {
+		printf("Input Directory entry at %s was neither a file or directory", srcdir);
+	}
+
+	return 0;
+}
---
+++ marker/os-244/warmup/wc.c
@@ -0,0 +1,126 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "common.h"
+#include "wc.h"
+#include <string.h>
+
+typedef struct Node {
+	char *word;
+	int count;
+	struct Node *next;
+} Node;
+
+struct wc {
+	/* you can define this struct to have whatever fields you want. */
+	Node **array; // array of linkedlistNodes
+	int size;
+	
+};
+
+int hash_func(const char *word, int size) {
+	unsigned long hash = 5381;
+	int c;
+	while ((c = *word++))
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	return hash % size;
+}
+
+
+
+Node * linear_search(Node *head, char *word){
+	while (head) {
+		if (strcmp(word, head->word) == 0)
+			return head;
+		head = head->next;
+	}
+	return NULL;
+}
+
+struct wc *
+wc_init(char *word_array, long size)
+{	
+	// initialize hash table
+	struct wc *wc;
+	int ht_size = size/2;
+
+
+	wc = (struct wc *)malloc(sizeof(struct wc));
+	wc->size = ht_size;
+	// wc->array = (Node **)calloc(ht_size, sizeof(Node));
+	wc->array = (Node **)calloc(ht_size, sizeof(Node *));
+	assert(wc);
+
+	// parse words and insert into hash table
+	char *wa_copy, *token;
+
+	wa_copy = strdup(word_array);
+
+	while( (token = strsep(&wa_copy," \n\r\t\v\f")) != NULL ) {
+		//=>Check if word exists in hashtable
+
+		// if the word is empty, ignore
+		if(strlen(token) == 0) continue;
+
+		int k = hash_func(token, ht_size);
+		// get the linked list at index k of the hash table
+		Node *head = wc->array[k];
+		Node *element = linear_search(head, token);
+		if (element) {
+			element->count += 1;
+		} else {
+
+			// initialize the new node
+			Node *entry = malloc(sizeof(Node));
+			entry->count = 1;
+			entry->word = strdup(token);
+			entry->next = head;
+
+			// insert entry at the head of wc[k]
+			wc->array[k] = entry;
+		}
+
+	}
+
+	return wc;
+}
+
+void
+wc_output(struct wc *wc)
+{
+	for(int i = 0; i<wc->size; i++){
+		Node *ptr = wc->array[i];
+
+		while(ptr) {
+			printf("%s:%d\n", ptr->word, ptr->count);
+			ptr = ptr->next;
+		}
+	}
+	
+}
+
+void
+list_destroy(Node *head) {
+	if(head == NULL) return;
+	if(head->next == NULL) {
+		free(head->word);
+		free(head);
+		return;
+	}
+	list_destroy(head->next);
+	free(head->word);
+	free(head);
+	return;
+}
+
+void
+wc_destroy(struct wc *wc)
+{
+	// loop through the entire array
+	for(int i  = 0; i < wc->size; ++i) {
+		// free each node
+		list_destroy(wc->array[i]);
+	}
+
+	free(wc);
+};;
